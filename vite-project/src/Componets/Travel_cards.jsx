54c39f0b3b1b8baa27953cc0b6df45ba52733360
JanLascala <lascala.jan@gmail.com>
2025-04-24 16:55:23 +0200

    Add test header to TravelCards component

--- vite-project/src/Componets/Travel_cards.jsx
+++ vite-project/src/Componets/Travel_cards.jsx
@@ -3,6 +3,8 @@ export default function TravelCards() {
         <div>
             <h1>this is travel cards</h1>
             <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias porro recusandae nam quibusdam illum alias ab cum voluptates? Sint dolorem obcaecati consectetur sequi adipisci, quo aut vel consequatur laborum in perspiciatis voluptas placeat saepe ea ipsam, ad non deserunt distinctio! Blanditiis dicta iure natus non facilis maxime culpa rem nemo!</p>
+
+            <h1>questo è un test</h1>
         </div>
     )
 };
