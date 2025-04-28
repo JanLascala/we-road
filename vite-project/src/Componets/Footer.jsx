170180fb03bd7b791ba3c85f248328f66a0389ed
Martina-dinapoli98 <martinadinapoli1998@gmail.com>
2025-04-28 13:29:44 +0200

    Refactor Footer component to improve layout and styling; update background color and remove unnecessary elements for a cleaner design.

--- vite-project/src/Componets/Footer.jsx
+++ vite-project/src/Componets/Footer.jsx
@@ -1,28 +1,13 @@
 export default function Footer() {
 
     return (
-        <footer className="bg-secondary mt-5 p-4">
-            <div className="container d-flex justify-content-around align-items-center gap-3 bg-secondary">
-                <div className="row">
-                    <div className="col-6">
-                        <ul className="list-unstyled">
-                            <li><a href="#"><h4>Link utilities</h4></a></li>
-                            <li><a href="#">Contacts</a></li>
-                            <li><a href="#">Tutorial</a></li>
-                            <li><a href="#">Privacy</a></li>
-                            <li><a href="#">Lorem</a></li>
-                        </ul>
-                    </div>
-                    <div className="col-6">
-                        <p>
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum deserunt minus quas magnam aliquam dolor dolorem voluptates aperiam iste. Necessitatibus fugiat aperiam impedit sapiente inventore! Dicta provident minus cumque tempore.
-                        </p>
-                    </div>
+        <>
+            <footer className="bg-light py-3 mt-4">
+                <div className="container text-center">
+                    <p>&copy; This site is owned by WeBool Italia S.r.l. - Viale Torino, 30 - 20143 Milano (MI) | Cod. fiscale e P.IVA 123456789 | Registro Imprese Milano 05/07/2022 n°987654321 | Num R.E.A.: MI-123567 WeRoad Italia S.r.l. società a socio unico.</p>
                 </div>
-            </div>
-            <p className="copyright text-muted m-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quos, magni ullam quidem cumque nihil, earum id error obcaecati quisquam veritatis, iusto magnam. Alias reiciendis suscipit dolorum, laborum nam totam!</p>
-
-        </footer>
+            </footer>
+        </>
     )
 
 }
